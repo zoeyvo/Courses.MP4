14e6778e41b3a1485bba6aaae8138c30d4ffe54e
Myranea <charleszhang03@gmail.com>
2025-06-01 17:48:01 -0700

    Limited keyword extraction to 10 most relevant phrases

--- backend/extract_keywords/README_KEYWORD_EXTRACT.md
+++ backend/extract_keywords/README_KEYWORD_EXTRACT.md
@@ -1,5 +1,8 @@
-Before running this webapp, ensure that your machine has NLTK's data installed. This data can be downloaded
-by installing nltk on your venv and using the command python -m nltk.downloader all
-
 The method can be found in the extract_keywords.py file; import the method as
 from extract_keywords.extract_keywords import extract_keywords
+
+Usage: extract_keywords(\<course description as string\>)
+
+Required packages: 
+scikit-learn
+sentence-transformers
--- backend/extract_keywords/extract_keywords.py
+++ backend/extract_keywords/extract_keywords.py
@@ -1,14 +1,27 @@
-import nltk
 import string
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import CountVectorizer
 
 def extract_keywords(desc : str):
     # Extracts keywords from the passed course description
     # @param desc: the course description
     # @returns a list of keywords
-    remove_punct = str.maketrans('', '', string.punctuation)
-    words = desc.split()
-    words = [w.translate(remove_punct) for w in words]
-    words = [word.lower() for word in words]
-    stop_words = set(nltk.corpus.stopwords.words('english'))
-    words = [w for w in words if not w in stop_words]
-    return words
+
+    # referenced from Maarten Grootendorst's guide on keyword extraction
+    n_gram_range = (1, 2)
+    stop_words = "english"
+
+    count = CountVectorizer(ngram_range = n_gram_range, stop_words=stop_words).fit([desc])
+    candidates = count.get_feature_names_out()
+
+    model = SentenceTransformer('distilbert-base-nli-mean-tokens')
+    doc_embedding = model.encode([desc])
+    candidate_embeddings = model.encode(candidates)
+
+    top_n = 10
+    distances = cosine_similarity(doc_embedding, candidate_embeddings)
+    keywords = [candidates[index] for index in distances.argsort()[0][-top_n:]]
+
+    return keywords
+
